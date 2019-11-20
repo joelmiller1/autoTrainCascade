bec0923dd55df060b03dc956ae7495e5705a2bec
Joel Miller <joel.miller.10@us.af.mil>
2019-11-20 16:02:43 -0600

    added multi-video capabilities

--- automate.py
+++ automate.py
@@ -4,45 +4,32 @@ Created on Tue Nov  5 10:38:16 2019
 
 @author: Joel Miller
 """
-import cv2,glob,os,subprocess,shlex,shutil
+import cv2,os,subprocess,shlex,shutil
 from selector import areaSelector, mask2Rect, box2Rect, mask2Box, box2Mask
 
 pathCount = lambda path: len([f for f in os.listdir(path)if os.path.isfile(os.path.join(path, f))])
 
 def main():
-    video_location = '../vids/f35-2.mp4'
-    #makeDirs()
-    #posCount,negCount = selectPosNeg(video_location)
-    objTrack(video_location)
-    createSamples()
-    trainCascade()
+    video_location = ['../vids/mario1.mp4',
+                      '../vids/mario3.mp4']
+    #selectPosNeg(video_location)
+    #objTrack(video_location)
+    #createSamples()
+    #trainCascade()
     playVids(video_location)
 
-def picList(picPath):
-    imgList = []
-    currentDir = os.getcwd()
-    os.chdir(picPath)   
-    for file in glob.glob('*.jpg'):
-        imgList.append(file)
-    os.chdir(currentDir)
-    count = len(imgList)
-    return count, imgList
-
 def makeDirs():
-    try:
-        file.close()
-        file2.close()
-    except:
-        print("Creating Directory")
+    print("Creating Temporary Directories")
     if os.path.isdir('data') == False:
         os.mkdir('data')
-    else:
-        shutil.rmtree('data')            
-    if os.path.isdir('data/pos') == False:
         os.mkdir('data/pos')
-    if os.path.isdir('data/neg') == False:
         os.mkdir('data/neg')
-
+    else:
+        shutil.rmtree('data')
+        os.mkdir('data')
+        os.mkdir('data/pos')
+        os.mkdir('data/neg')
+        
 def objTrack(videoLocation):
     cap = cv2.VideoCapture(videoLocation)
     cap.set(cv2.CAP_PROP_POS_MSEC,90*1000)
@@ -53,8 +40,8 @@ def objTrack(videoLocation):
     posCount = 0
     negCount = 0
     makeDirs()
-    bgFile = open('data/bg.txt','w+')
-    datFile = open('data/info.dat','w+')
+    dat = []
+    bg = []
     
     while True:
         success,frame = cap.read()
@@ -70,11 +57,11 @@ def objTrack(videoLocation):
             mask = box2Mask(box)
             posCount += 1
             cv2.imwrite(f'data/pos/pos{posCount}.jpg',refFrame[mask])
-            datFile.write(f'pos/pos{posCount}.jpg  1  0 0 {int(box[2])} {int(box[3])}\n')
+            dat.append(f'pos/pos{posCount}.jpg  1  0 0 {int(box[2])} {int(box[3])}\n')
             refFrame = cv2.rectangle(refFrame,rectPts[0],rectPts[1],(0,0,0),cv2.FILLED)
             negCount += 1
             cv2.imwrite(f'data/neg/neg{negCount}.jpg',refFrame)
-            bgFile.write(f'neg/neg{negCount}.jpg\n')
+            bg.append(f'neg/neg{negCount}.jpg\n')
         else:
             box = mask2Box(areaSelector(refFrame))
             tracker = cv2.TrackerMedianFlow_create()
@@ -85,88 +72,100 @@ def objTrack(videoLocation):
         if key == ord('q'):
             break
     
+    # write out background file
+    bgFile = open('data/bg.txt','w+')
+    [bgFile.write(i) for i in bg]
     bgFile.close()
+    # write out dat file
+    datFile = open('data/info.dat','w+')
+    [datFile.write(i) for i in dat]
     datFile.close()
+    
     cap.release()
     cv2.destroyAllWindows()
     f'Positive Count: {posCount} Negative Count: {negCount}'
 
 def bgList(vidPath):
-    vidcap = cv2.VideoCapture(vidPath)
     if os.path.isdir('data/raw') == False:
         os.mkdir('data/raw')
-    count, imgList = picList('data/raw')
+    count = pathCount('data/raw')
+    # it will capture image in each 5 seconds
+    frameRate = 1
     
-    def getFrame(sec):
-        vidcap.set(cv2.CAP_PROP_POS_MSEC,sec*1000)
-        hasFrames,image = vidcap.read()
-        if hasFrames:
-            cv2.imwrite("data/raw/img"+str(count+1)+".jpg", image)     # save frame as JPG file
-        return hasFrames,image
-    
-    sec = 2
-    frameRate = 5 #//it will capture image in each 0.5 second
-    success,image  = getFrame(sec)
-    
-    while success:
-        count = count + 1
-        sec = sec + frameRate
-        sec = round(sec, 2)
-        success,image = getFrame(sec)
-
-        if (count%50) == 0:
-            print("converting images...")
+    for i in list(range(0,len(vidPath))):
+        sec = 2
+        vidcap = cv2.VideoCapture(vidPath[i])   
+        success,image = vidcap.read()
+        while success:
+            vidcap.set(cv2.CAP_PROP_POS_MSEC,sec*1000)
+            success,image = vidcap.read()
+            if success:
+                cv2.imwrite(f'data/raw/img{count+1}.jpg', image)
+                count += 1
+            sec += frameRate
+            if (count%50) == 0:
+                print("converting images...")
+                
     vidcap.release()
     cv2.destroyAllWindows()
     print("Finished making pictures")
         
 def selectPosNeg(video_location):
+    makeDirs()
     bgList(video_location)
-    count, imgList = picList('data/raw')
-    file = open('data/bg.txt','w+')
-    file2 = open('data/info.dat','w+')
-    i = 0
+    count = pathCount('data/raw')
+    i = 1
     posCount = 1
+    negCount = 1
+    posList = []
+    negList = []
     
-    while i < (count):
-        refImg = cv2.imread('data/raw/'+imgList[i])
+    while i <= count:
+        refImg = cv2.imread(f'data/raw/img{i}.jpg')
         modImg = refImg.copy()
-        print("Image "+str(i+1)+"/"+str(count))
         box = areaSelector(modImg)
         if box[0] == -1:
-            cv2.imwrite('data/neg/'+imgList[i],modImg)
-            file.write('neg/'+imgList[i]+'\n')
+            cv2.imwrite(f'data/neg/neg{negCount}.jpg',modImg)
+            negList.append(f'neg/neg{negCount}.jpg\n')
+            negCount += 1
         elif box[0] == -9:
             i -= 2
         elif box[0] == -21:
             i += 1
             continue
         elif box[0] == -31:
-            file.close()
-            file2.close()
             break
         else:
             modImg = modImg[box]
-            cv2.imwrite('data/pos/pos'+str(posCount)+'.jpg',modImg)
+            cv2.imwrite(f'data/pos/pos{posCount}.jpg',modImg)
             width = modImg.shape[1]
             height = modImg.shape[0]
-            file2.write(f'pos/pos{posCount}.jpg  1  0 0 {width} {height}\n')
+            posList.append(f'pos/pos{posCount}.jpg  1  0 0 {width} {height}\n')
             posCount += 1
             temp = mask2Rect(box)
             refImg = cv2.rectangle(refImg,temp[0],temp[1],(0,0,0),cv2.FILLED)
-            cv2.imwrite('data/neg/'+imgList[i],refImg)
-            file.write('neg/'+imgList[i]+'\n')
+            cv2.imwrite(f'data/neg/neg{negCount}.jpg\n',refImg)
+            negList.append('neg/neg{negCount}.jpg\n')
+            negCount += 1
         f'Image {i}/{count}'
         i += 1
-    file.close()
-    file2.close()
+    
+    # Negative file list creation
+    bgFile = open('data/bg.txt','w+')
+    [bgFile.write(i) for i in negList]
+    bgFile.close()
     print('Background text file written')
+    
+    # Positive file list creation
+    datFile = open('data/info.dat','w+')
+    [datFile.write(i) for i in posList]
+    datFile.close()
     print('info.dat file written')
-    negCount,qq = picList('data/neg')
+    
+    negCount = pathCount('data/neg')
     print('Number of Negative pics: '+str(negCount))
-    posCount,qq = picList('data/pos')
+    posCount = pathCount('data/pos')
     print('Number of Positive pics: '+str(posCount))
-    return posCount, negCount
     
 def createSamples():
     print('Beginning vec file creation')
@@ -192,9 +191,7 @@ def trainCascade():
 
 def playVids(video_location):
     usps_cascade = cv2.CascadeClassifier('data/cascade.xml')
-    cap = cv2.VideoCapture(video_location)
-    cap.set(1,24*30)
-    
+    cap = cv2.VideoCapture(video_location[0])
     ret, frame = cap.read()
     
     while(cap.isOpened()):
