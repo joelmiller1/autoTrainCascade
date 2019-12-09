02d187322a3e6f77235fa7767965c6a28b7979e4
Joel Miller <joel.miller.10@us.af.mil>
2019-12-09 15:50:35 -0600

    rough, working copy of gui program

--- gui.py
+++ gui.py
@@ -8,94 +8,80 @@ learning from this website:
 https://realpython.com/python-gui-with-wxpython/
 
 """
-import wx,glob
+import os,wx
+from automate import objTrack
+ 
+wildcard = "mp4 source (*.mp4)|*.mp4|" \
+            "All files (*.*)|*.*"
 
 
-class videoListPanel(wx.Panel):    
-    def __init__(self, parent):
-        super().__init__(parent)
-        main_sizer = wx.BoxSizer(wx.VERTICAL)
-        self.row_obj_dict = {}
-
-        self.list_ctrl = wx.ListCtrl(
-            self, size=(-1, 100), 
-            style=wx.LC_REPORT | wx.BORDER_SUNKEN
-        )
-        self.list_ctrl.InsertColumn(0, 'Video(s) Name', width=200)
-        self.list_ctrl.InsertColumn(1, 'Path', width=140)
-        main_sizer.Add(self.list_ctrl, 0, wx.ALL | wx.EXPAND, 5)        
-        edit_button = wx.Button(self, label='Edit')
-        edit_button.Bind(wx.EVT_BUTTON, self.on_edit)
-        main_sizer.Add(edit_button, 0, wx.ALL | wx.CENTER, 5)        
-        self.SetSizer(main_sizer)
-
-    def on_edit(self, event):
-        print('in on_edit')
-
-    def updateVideoList(self, folder_path):
-        print(folder_path)
-        
-        
-
-class MyFrame(wx.Frame):    
+class MyForm(wx.Frame):
+ 
+    #----------------------------------------------------------------------
     def __init__(self):
-        super().__init__(parent=None, title='Hello World')
-        # Windows needs this for Tab traversal
-        panel = wx.Panel(self)        
-        my_sizer = wx.BoxSizer(wx.VERTICAL)
+        wx.Frame.__init__(self, None, wx.ID_ANY,
+                          "Cascade Trainer")
+        panel = wx.Panel(self, wx.ID_ANY)
+        self.currentDirectory = os.getcwd()
+
+        # Title Text
+        titleText = wx.StaticText(panel,label='Location of video files to train')
         
-        # Add Text Input to panel -- first arg is where text goes on
-        self.text_ctrl = wx.TextCtrl(panel)
-        # Have text box dynamically sized
-        my_sizer.Add(self.text_ctrl, 0, wx.ALL | wx.EXPAND, 10)
+        # create text box
+        self.textBox = wx.TextCtrl(panel,size = (200,100), style=wx.TE_MULTILINE)
         
-        # Add Button to do something -- first arg is where btn goes
-        my_btn = wx.Button(panel, label='Press Me')
-        my_btn.Bind(wx.EVT_BUTTON, self.btnpress)
-        # Button is located beneath text box, centered
-        my_sizer.Add(my_btn, 0, wx.ALL | wx.CENTER, 10)
+        # create the buttons and bindings
+        openFileDlgBtn = wx.Button(panel, label="Select Files")
+        openFileDlgBtn.Bind(wx.EVT_BUTTON, self.onOpenFile)
         
-        panel.SetSizer(my_sizer)        
-        self.Show()
+        startTrain = wx.Button(panel, label="Start Training")
+        startTrain.Bind(wx.EVT_BUTTON, self.startTraining)
         
-    def btnpress(self, event):
-        value = self.text_ctrl.GetValue()
-        value = int(value)
-        a = [print(i) for i in list(range(0,value))]
-    
-    
-class mainFrame(wx.Frame):    
-    def __init__(self):
-        super().__init__(parent=None,
-                         title='Cascade Trainer')
-        self.panel = videoListPanel(self)
-        self.Show()
+        # Status Box
+        self.statusBox = wx.TextCtrl(panel,size = (200,100), style=wx.TE_MULTILINE)
+
+        # put the buttons in a sizer
+        vSize = wx.BoxSizer(wx.VERTICAL)
+        hSize = wx.BoxSizer(wx.HORIZONTAL)
+        vSize.Add(titleText,0, wx.ALL|wx.CENTER)
+        vSize.Add(self.textBox,0, wx.ALL|wx.CENTER|wx.EXPAND, 5)
         
-        def create_menu(self):
-            menu_bar = wx.MenuBar()
-            file_menu = wx.Menu()
-            open_folder_menu_item = file_menu.Append(
-                wx.ID_ANY, 'Open Folder', 
-                'Open a folder with MP3s'
-            )
-            menu_bar.Append(file_menu, '&File')
-            self.Bind(
-                event=wx.EVT_MENU, 
-                handler=self.on_open_folder,
-                source=open_folder_menu_item,
-            )
-            self.SetMenuBar(menu_bar)
-    
-        def on_open_folder(self, event):
-            title = "Choose a directory:"
-            dlg = wx.DirDialog(self, title, 
-                               style=wx.DD_DEFAULT_STYLE)
-            if dlg.ShowModal() == wx.ID_OK:
-                self.panel.update_mp3_listing(dlg.GetPath())
-            dlg.Destroy()
+        hSize.Add(openFileDlgBtn, 0, wx.ALL|wx.CENTER, 5)
+        hSize.Add(startTrain, 0, wx.ALL|wx.CENTER, 5)
+        vSize.Add(hSize)
+        
+        vSize.Add(self.statusBox,0, wx.ALL|wx.CENTER|wx.EXPAND, 5)
+        panel.SetSizer(vSize)
 
 
-if __name__ == '__main__':
-    app = wx.App()
-    frame = mainFrame()
+    #----------------------------------------------------------------------
+    def onOpenFile(self, event):
+        """
+        Create and show the Open FileDialog
+        """
+        dlg = wx.FileDialog(
+            self, message="Choose a file",
+            defaultDir=self.currentDirectory, 
+            defaultFile="",
+            wildcard=wildcard,
+            style=wx.FD_OPEN | wx.FD_MULTIPLE | wx.FD_CHANGE_DIR
+            )
+        if dlg.ShowModal() == wx.ID_OK:
+            self.filePaths = dlg.GetPaths()
+            a = dlg.GetPaths()
+            self.textBox.Clear()
+            for i in list(range(0,len(self.filePaths))):
+                self.textBox.AppendText(a[i]+'\n')
+        dlg.Destroy()
+    
+    def startTraining(self, event):
+        print(self.filePaths)
+        objTrack(self.filePaths)
+
+#----------------------------------------------------------------------
+# Run the program
+if __name__ == "__main__":
+    app = wx.App(False)
+    frame = MyForm()
+    frame.Show()
     app.MainLoop()
